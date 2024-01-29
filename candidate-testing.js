7451dc55d76790534eb7a14dfcec396dcbe1ef1e
austinrakey <rakeysound@gmail.com>
2024-01-28 20:28:59 -0600

    part 3 candidate testing FINISHED

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,8 +12,8 @@ let candidateAnswer = ' ';
 
 
 //TODO: Variables for Part 2
-let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "]
-let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"]
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
 
@@ -28,14 +28,15 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswers.push(input.question(questions[i]));
+  }
 // candidateAnswer = input.question("Who was the first American woman in space? ")
 
 }
 
-for (let i = 0; i < questions.length; i++) {
-  candidateAnswers.push(input.question(questions[i]));
-}
+
  //console.log(candidateAnswers)
 
 
@@ -45,16 +46,25 @@ for (let i = 0; i < questions.length; i++) {
 
 function gradeQuiz(candidateAnswers) {
 
+
+// let lowerCorrectAnswers = correctAnswers.map(toLowercase())
+// let lowerCandidateAnswers = candidateAnswers.map(toLowercase())
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-for (let i = 0; i < questions.length; i++)
-      if (correctAnswers[i] === candidateAnswers[i]) {
+let score = 0
+// let lowerCorrect =
+// let lowerCandidate = 
+  
+  for (let i = 0; i < questions.length; i++)
+      if (correctAnswers[i].toLowerCase() === candidateAnswers[i].toLowerCase()) {
       console.log(`Question ${i + 1} was correct!`);
+      score = score + 1
 } else {
       console.log(`Question ${i + 1} is incorrect you answered ${candidateAnswers[i]} but the correct answer was ${correctAnswers[i]}.`);
 }
 
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
+  let grade = (score*20);  //TODO 3.2 use this variable to calculate the candidates score.
 
+  console.log(`Your final score is ${grade}.`)
 
   return grade;
 }
